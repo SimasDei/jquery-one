1ef8050965ba7623c401805d0a99ecaffc191a59
SimasDei <simondeiman@gmail.com>
2019-04-18 21:59:59 +0300

    use jQuery animate property

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-$(document).ready(function() {});
+$(document).ready(function() {
+  $('.content-box').click(function() {
+    $('.content-box').animate(
+      {
+        width: '500px',
+        height: '+=250px'
+      },
+      200
+    );
+    $('.content-after')
+      .show()
+      .animate(
+        {
+          opacity: '1'
+        },
+        200
+      );
+  });
+});
