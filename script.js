8552018214fd730568a7a22dec64f4eec546db19
SimasDei <simondeiman@gmail.com>
2019-04-18 21:26:57 +0300

    add mini project markup

--- script.js
+++ script.js
@@ -1,14 +1,5 @@
 $(document).ready(function() {
-  $('#p1').hover(function() {
-    $(this).hide();
-  });
-  $('#p2').click(function() {
-    $(this).hide();
-  });
-  $('#p3').dblclick(function() {
-    $(this).hide();
-  });
-  $('#p4').mouseout(function() {
-    $(this).hide();
+  $('#menu-toggle').click(function() {
+    $('#side-nav').show();
   });
 });
