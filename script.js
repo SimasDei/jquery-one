bd6604f411f58620c2ae818f48c94ac8208efb31
SimasDei <simondeiman@gmail.com>
2019-04-18 21:11:43 +0300

    jQuery events

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
-$(document).ready(function () {
-  $('#test').click(function () {
-    alert('Clicked');
-  })
-})
+$(document).ready(function() {
+  $('#p1').hover(function() {
+    $(this).hide();
+  });
+  $('#p2').click(function() {
+    $(this).hide();
+  });
+  $('#p3').dblclick(function() {
+    $(this).hide();
+  });
+  $('#p4').mouseout(function() {
+    $(this).hide();
+  });
+});
